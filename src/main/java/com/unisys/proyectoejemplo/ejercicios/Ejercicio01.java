2e078d094a0e33b84da9225fe4fe33bdeba7a392
AHL1966 <alfonsohernandez1966@gmail.com>
2023-01-16 18:29:52 +0100

    Primeros pasos ejercicio 1

---
+++ src/main/java/com/unisys/proyectoejemplo/ejercicios/Ejercicio01.java
@@ -0,0 +1,27 @@
+package com.unisys.proyectoejemplo.ejercicios;
+
+/*
+entero llamado numero,
+corto llamado marianico,
+largo llamado gasol,
+booleano llamado independencia,
+double llamado presupuesto y
+float llamado longitud.
+Declara una variable llamada suma: que sea la suma de gasol y marianico, y sácala por pantalla
+ */
+
+public class Ejercicio01 {
+
+
+    public static void main(String[] args) {
+        int numero;
+        short marianico=10;
+        long gasol= 1000;
+        boolean independencia;
+        double presupuesto;
+        float longitud;
+        int suma;
+        suma = (int)gasol + marianico;
+        System.out.println("Contenido de suma "+suma);
+    }
+}
